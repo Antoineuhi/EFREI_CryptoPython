a25bde45556afa98c76c195f9439516b044f1e0e
Antoineuhi <101323204+Antoineuhi@users.noreply.github.com>
2025-05-22 18:20:52 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bienvenue sur l'app web de chiffrement/déchiffrement! Utilisez /encrypt/<clé>/<valeur> ou /decrypt/<clé>/<valeur>"
+    return "Bienvenue sur l'app web de Antoine D de chiffrement/déchiffrement! Utilisez /encrypt/<clé>/<valeur> ou /decrypt/<clé>/<valeur>"
 
 @app.route('/encrypt/<string:cle>/<string:valeur>')
 def encrypt_personnalise(cle, valeur):
